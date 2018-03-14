9fabfef24b20f7916bdc0ca60f47fc8010c0e43e
Jannik Buhr <jannik.m.buhr@gmail.com>
2018-03-14 16:03:55 +0100

    updated run

--- runShinyWrangler.R
+++ runShinyWrangler.R
@@ -1,6 +1,5 @@
 # If you don't have the following packages installed, the app will do so for you:
-# shiny, shinydashboard, tidyverse, DT, minpack.lm, broom, modelr
-
+# shiny, shinydashboard, tidyverse, DT, readxl
 
 # Just run these two lines
 # -> You need to install the shiny package first with install.packages("shiny")
